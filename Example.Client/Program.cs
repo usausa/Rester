5dfe85de1ebd8fd5135e737c66be9d033528be3a
usausa <machi.pon@gmail.com>
2019-04-20 21:42:27 +0900

    Add example baseline

---
+++ Example.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.Client
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ Example.Server/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace Example.Server.Controllers
+{
+    using System.Diagnostics;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Example.Server.Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
